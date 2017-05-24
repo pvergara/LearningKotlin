daa0c1e4cd930d7653cae33e25a0684f88712daa
Pablo Cristóbal Vergara Castro <pablocristobal@gmail.com>
2017-05-25 00:41:56 +0200

    init

---
+++ main/kotlin/StringTemplates.kt
@@ -0,0 +1,15 @@
+package org.ecos.learning.kotlin
+
+class StringTemplates {
+    fun main(): Pair<String, String> {
+        var a = 1
+        // simple name in template:
+        val s1 = "a is $a"
+        a = 2
+        // arbitrary expression in template:
+
+        val s2 = "${s1.replace("is", "was")}, but now is $a"
+
+        return Pair(s1,s2);
+    }
+}
---
+++ test/StringTemplatesTests.kt
@@ -0,0 +1,17 @@
+import org.ecos.learning.kotlin.StringTemplates
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Test
+
+
+class StringTemplatesTests{
+    @Test fun test(){
+        val templates = StringTemplates()
+        val (a,b) = templates.main()
+
+        assertThat(a, `is`(equalTo("a is 1")))
+        assertThat(b, `is`(equalTo("a was 1, but now is 2")))
+    }
+
+}
